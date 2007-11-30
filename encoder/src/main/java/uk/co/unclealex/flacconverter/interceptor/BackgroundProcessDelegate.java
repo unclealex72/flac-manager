39f57434356d26a7475d0298221f4b4e79efac2c
Alex Jones <alex.jones@unclealex.co.uk>
2007-11-30 14:50:19 +0000

    Splitting the project so that the encoding part is seperate to the web interface parts.

--- encoder/src/main/java/uk/co/unclealex/flacconverter/interceptor/BackgroundProcessDelegate.java
+++
@@ -1,9 +0,0 @@
-package uk.co.unclealex.flacconverter.interceptor;
-
-import com.opensymphony.xwork2.ActionInvocation;
-
-public interface BackgroundProcessDelegate {
-
-	public void beforeInvocation(ActionInvocation invocation, Object action) throws Exception;
-	public void afterInvocation(ActionInvocation invocation, Object action) throws Exception;
-}
