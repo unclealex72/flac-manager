6fc88f158622d3fa4bc5c1d4b8168cef544dcc25
Alex Jones <alex.jones@unclealex.co.uk>
2007-12-11 22:26:06 +0000

    Splitting initialisation into model initialising and track initialising.

---
+++ core/src/main/java/uk/co/unclealex/music/core/initialise/Initialiser.java
@@ -0,0 +1,16 @@
+package uk.co.unclealex.music.encoder.encoded.initialise;
+
+
+import java.io.IOException;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface Initialiser {
+
+	public void initialise() throws IOException;
+	
+	public void importTracks() throws IOException;
+
+	public void clear();	
+}
