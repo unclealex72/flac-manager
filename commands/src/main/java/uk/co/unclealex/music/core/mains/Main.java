a7611652462d2d0a62a00f0ceadb3263fdf45ed6
Alex Jones <alex.jones@unclealex.co.uk>
2007-12-26 20:53:25 +0000

    A new project used to house all music commands.

---
+++ commands/src/main/java/uk/co/unclealex/music/core/mains/Main.java
@@ -0,0 +1,22 @@
+package uk.co.unclealex.music.core.mains;
+
+import java.util.List;
+
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public abstract class Main {
+
+	public abstract void execute() throws Exception;
+	
+	protected abstract List<String> getContextLocations();
+	
+	public static void execute(Main main) throws Exception {
+		ApplicationContext applicationContext =
+			new ClassPathXmlApplicationContext(main.getContextLocations().toArray(new String[0]));
+		applicationContext.getAutowireCapableBeanFactory().autowireBeanProperties(
+				main, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
+		main.execute();
+	}
+}
