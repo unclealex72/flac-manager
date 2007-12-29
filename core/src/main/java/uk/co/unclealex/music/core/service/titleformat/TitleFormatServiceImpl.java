3ff24c52507167ff83ebfd8e46d2c2d3d2aaf03e
Alex Jones <alex.jones@unclealex.co.uk>
2007-12-29 00:22:00 +0000

    Refactored filename creation into its own service bean.

--- core/src/main/java/uk/co/unclealex/music/core/service/titleformat/TitleFormatServiceImpl.java
+++ core/src/main/java/uk/co/unclealex/music/core/service/titleformat/TitleFormatServiceImpl.java
@@ -1,13 +1,5 @@
 package uk.co.unclealex.music.core.service.titleformat;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Required;
-
-import uk.co.unclealex.music.core.SlimServerConfig;
 import uk.co.unclealex.music.core.model.EncodedAlbumBean;
 import uk.co.unclealex.music.core.model.EncodedArtistBean;
 import uk.co.unclealex.music.core.model.EncodedTrackBean;
@@ -17,54 +9,20 @@ import uk.co.unclealex.music.core.substitutor.Substitutor;
 @Prototype
 public class TitleFormatServiceImpl implements TitleFormatService {
 
-	private static String INVALID_CHARACTER_STRING = "/\\?%*:|\"<>";
-	protected static final List<String> INVALID_CHARACTERS;
-	static {
-		List<String> invalidCharacters = new LinkedList<String>();
-		for (Character c : INVALID_CHARACTER_STRING.toCharArray()) {
-			invalidCharacters.add(c.toString());
-		}
-		INVALID_CHARACTERS = Collections.unmodifiableList(invalidCharacters);
-	}
-
 	private String i_titleFormat;
-	private SlimServerConfig i_slimServerConfig;
 
 	public String getTitle(EncodedTrackBean trackBean) {
 		Substitutor substitutor = new Substitutor(getTitleFormat());
 		EncodedAlbumBean albumBean = trackBean.getEncodedAlbumBean();
 		EncodedArtistBean artistBean = albumBean.getEncodedArtistBean();
 		substitutor.substitute(TitleFormatVariable.TRACK, trackBean.getTrackNumber());
-		substitutor.substitute(TitleFormatVariable.TITLE, sanitise(trackBean.getTitle()));
-		substitutor.substitute(TitleFormatVariable.ALBUM, sanitise(albumBean.getTitle()));
-		substitutor.substitute(TitleFormatVariable.ARTIST, sanitise(removeDefiniteArticle(artistBean.getName())));
+		substitutor.substitute(TitleFormatVariable.TITLE, trackBean.getFilename());
+		substitutor.substitute(TitleFormatVariable.ALBUM, albumBean.getFilename());
+		substitutor.substitute(TitleFormatVariable.ARTIST, artistBean.getFilename());
 		substitutor.substitute(TitleFormatVariable.EXTENSION, trackBean.getEncoderBean().getExtension());
 		return substitutor.getText();
 	}
 
-	protected String sanitise(String str) {
-		for (String c : INVALID_CHARACTERS) {
-			str = StringUtils.replace(str, c, "");
-		}
-		return str;
-	}
-	
-	protected String removeDefiniteArticle(String artist) {
-		List<String> definiteArticles = getSlimServerConfig().getDefiniteArticles();
-		if (definiteArticles == null) {
-			return artist;
-		}
-		for (String article : definiteArticles) {
-			article = article.trim();
-			int toRemove = article.length() + 1;
-			if (artist.startsWith(article) && artist.length() > toRemove && 
-					Character.isWhitespace(artist.charAt(toRemove - 1))) {
-				return artist.substring(toRemove).trim();
-			}
-		}
-		return artist;
-	}
-	
 	public String getTitleFormat() {
 		return i_titleFormat;
 	}
@@ -72,14 +30,4 @@ public class TitleFormatServiceImpl implements TitleFormatService {
 	public void setTitleFormat(String titleFormat) {
 		i_titleFormat = titleFormat;
 	}
-
-	public SlimServerConfig getSlimServerConfig() {
-		return i_slimServerConfig;
-	}
-
-	@Required
-	public void setSlimServerConfig(SlimServerConfig slimServerConfig) {
-		i_slimServerConfig = slimServerConfig;
-	}
-
 }
