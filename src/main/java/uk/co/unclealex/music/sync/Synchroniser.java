47558ece5a5e0b067412800b9a952079bde2ebc1
Alex Jones <alex.jones@unclealex.co.uk>
2012-03-11 19:56:40 +0000

    Moving all code to a "legacy" directory.

--- src/main/java/uk/co/unclealex/music/sync/Synchroniser.java
+++
@@ -1,9 +0,0 @@
-package uk.co.unclealex.music.sync;
-
-import java.io.IOException;
-
-public interface Synchroniser {
-
-	public void synchronise() throws IOException;
-
-}
--- src/main/java/uk/co/unclealex/music/sync/SynchroniserFactory.java
+++
@@ -1,9 +0,0 @@
-package uk.co.unclealex.music.sync;
-
-import uk.co.unclealex.music.Device;
-
-public interface SynchroniserFactory {
-
-	public Synchroniser createSynchroniser(Device device);
-
-}
