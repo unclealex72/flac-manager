ba761e2f9d483b4ccf8e58eaa9afa6ec58c92857
Alex Jones <alex.jones@unclealex.co.uk>
2012-11-04 15:54:12 +0000

    Added a factory for file locations.

---
+++ src/test/java/uk/co/unclealex/music/files/FileLocationFactoryImplTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2012 Alex Jones
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ *
+ * @author unclealex72
+ *
+ */
+
+package uk.co.unclealex.music.files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import uk.co.unclealex.music.configuration.Directories;
+import uk.co.unclealex.music.configuration.json.PathsBean;
+
+/**
+ * @author alex
+ * 
+ */
+public class FileLocationFactoryImplTest {
+
+  Directories directories = new PathsBean(
+      Paths.get("/flacPath"),
+      Paths.get("/devicesPath"),
+      Paths.get("/encodedPath"),
+      Paths.get("/stagingPath"));
+
+  FileLocationFactory fileLocationFactory = new FileLocationFactoryImpl(directories);
+
+  @Test
+  public void testFlac() {
+    test(
+        Paths.get("/flacPath"),
+        Paths.get("my", "flac", "file.flac"),
+        fileLocationFactory.createFlacFileLocation(Paths.get("my", "flac", "file.flac")));
+  }
+
+  @Test
+  public void testDevices() {
+    test(
+        Paths.get("/devicesPath"),
+        Paths.get("device", "my", "device", "file.flac"),
+        fileLocationFactory.createDeviceFileLocation("device", Paths.get("my", "device", "file.flac")));
+  }
+
+  @Test
+  public void testEncoded() {
+    test(
+        Paths.get("/encodedPath"),
+        Paths.get("my", "encoded", "file.mp3"),
+        fileLocationFactory.createEncodedFileLocation(Paths.get("my", "encoded", "file.mp3")));
+  }
+
+  @Test
+  public void testStaging() {
+    test(
+        Paths.get("/stagingPath"),
+        Paths.get("my", "flac", "file.flac"),
+        fileLocationFactory.createStagingFileLocation(Paths.get("my", "flac", "file.flac")));
+  }
+
+  protected void test(Path expectedBasePath, Path expectedRelativePath, FileLocation actualFileLocation) {
+    assertEquals(
+        "The wrong file location was created.",
+        new FileLocation(expectedBasePath, expectedRelativePath),
+        actualFileLocation);
+  }
+}
