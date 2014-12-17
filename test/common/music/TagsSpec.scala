ef3bf49e251be907cc9e688650c1b6aaddfddd41
Alex Jones <alex.jones@unclealex.co.uk>
2014-12-17 18:49:35 +0000

    Update copyright notices.

--- test/common/music/TagsSpec.scala
+++ test/common/music/TagsSpec.scala
@@ -1,34 +1,26 @@
-/**
- * Copyright 2011 Alex Jones
+/*
+ * Copyright 2014 Alex Jones
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- * @author unclealex72
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package common.music
 
 import java.nio.file.Paths
 
+import com.wix.accord._
 import common.files.FLAC
 import org.specs2.mutable._
-import com.wix.accord._
 
 /**
  * @author alex
