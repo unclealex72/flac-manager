ab324a20dc65b171b4d929f0c09eab7804e6043a
Alex Jones <alex.jones@unclealex.co.uk>
2017-05-13 17:00:11 +0100

    Increase size of database columns.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,8 +1,8 @@
 # -- Schema creation
 # --- !Ups
 create table `change` (
-  relativePath varchar(128) not null,
-  parentRelativePath varchar(128),
+  relativePath varchar(256) not null,
+  parentRelativePath varchar(256),
   at timestamp not null,
   id bigint not null primary key auto_increment,
   action varchar(128) not null,
